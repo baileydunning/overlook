f10990be35df0d397d8a082c39d18cc2c0f9fa5c
Bailey Dunning <bwdunning@gmail.com>
2020-11-04 16:48:35 -0700

    Create method and test for createBookingsRecord

--- src/data-model/bookingRecord.js
+++ src/data-model/bookingRecord.js
@@ -3,12 +3,15 @@ import Booking from './booking'
 export default class BookingRecord {
   constructor(bookingData) {
     this.rawBookingData = bookingData,
-    this.bookingRecord = []
-    this.currentBookings = []
+    this.bookingRecord = [],
+    this.currentBookings = [],
     this.previousBookings = []
   }
 
   createBookingsRecord() {
-
+    this.bookingRecord = this.rawBookingData.reduce((acc, booking) => {
+      acc.push(new Booking(booking));
+      return acc
+    }, []);
   }
 }
--- test/bookingRecord-test.js
+++ test/bookingRecord-test.js
@@ -4,7 +4,7 @@ import {sampleTestData} from './sampleTestData.js'
 import Booking from '../src/data-model/booking';
 import BookingRecord from '../src/data-model/bookingRecord';
 
-describe('BookingRecord', () => {
+describe.only('BookingRecord', () => {
   let bookingRecord;
   beforeEach(() => {
     bookingRecord = new BookingRecord(sampleTestData.bookingData)
@@ -13,6 +13,24 @@ describe('BookingRecord', () => {
   describe('Constructor', () => {
     it('should be a function', () => {
       expect(BookingRecord).to.be.a('function');
+    });
+
+    it('should create an instance of BookingRecord', () => {
+      expect(bookingRecord).to.be.an.instanceof(BookingRecord);
+    });
+
+    it('should take in the booking data', () => {
+      expect(bookingRecord.rawBookingData).to.deep.equal(sampleTestData.bookingData)
+    });
+  });
+
+  describe('Methods', () => {
+    beforeEach(() => {
+      bookingRecord.createBookingsRecord();
+    });
+
+    it('should put Bookings in the booking record', () => {
+      expect(bookingRecord.bookingRecord[0]).to.be.an.instanceof(Booking);
     });
   });
 });
