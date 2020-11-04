4b6cd9afa49de72c1042e44bfdb266b9a9ae28a5
Bailey Dunning <bwdunning@gmail.com>
2020-11-04 16:00:46 -0700

    Add constructor tests for Booking

--- src/data-model/booking.js
+++ src/data-model/booking.js
@@ -3,7 +3,7 @@ export default class Booking {
     this.id = booking.id,
     this.userID = booking.userID,
     this.date = booking.date,
-    this.roomNumber = booking.date,
+    this.roomNumber = booking.roomNumber,
     this.roomServiceCharges = []
   }
 }
--- test/booking-test.js
+++ test/booking-test.js
@@ -2,8 +2,45 @@ import chai from 'chai';
 const expect = chai.expect;
 import Booking from '../src/data-model/booking';
 
-describe('Booking', () => {
-  it('should return true', () => {
-    expect(true).to.equal(true);
+describe.only('Booking', () => {
+  let booking;
+  beforeEach(() => {
+    booking = new Booking({
+      "id": "5fwrgu4i7k55hl6t5",
+      "userID": 1,
+      "date": "2020/11/03",
+      "roomNumber": 24
+    })
+  })
+
+  describe('Constructor', () => {
+
+    it('should be a function', () => {
+      expect(Booking).to.be.a('function');
+    });
+
+    it('should create an instance of booking', () => {
+      expect(booking).to.be.an.instanceof(Booking)
+    });
+
+    it('should have an id', () => {
+      expect(booking.id).to.deep.equal("5fwrgu4i7k55hl6t5");
+    });
+
+    it('should have a user id', () => {
+      expect(booking.userID).to.deep.equal(1);
+    });
+
+    it('should have a date', () => {
+      expect(booking.date).to.deep.equal("2020/11/03");
+    });
+
+    it('should have a room number', () => {
+      expect(booking.roomNumber).to.deep.equal(24);
+    });
+
+    it('should be instantiated with no room service charges', () => {
+      expect(booking.roomServiceCharges).to.deep.equal([]);
+    });
   });
 });
