6b1192a4adc5aae5f3c18f9d4ed1c9463a95428c
Bailey Dunning <bwdunning@gmail.com>
2020-11-04 14:41:49 -0700

    Add login screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './css/styles.scss';
+import {loginButton, loginView, roomsContainer, sidebar, userDashboard} from './elements.js';
+
+loginButton.addEventListener('click', determineUser)
+
+function determineUser() {
+  loginView.classList.add('hidden');
+  userDashboard.classList.remove('hidden');
+  sidebar.classList.remove('hidden');
+  roomsContainer.classList.remove('hidden');
+}
