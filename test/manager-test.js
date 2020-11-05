903e8af08bde4956a15007ce374f8e2a7026176d
Bailey Dunning <bwdunning@gmail.com>
2020-11-04 19:16:05 -0700

    Add manager tests for constructor

--- test/manager-test.js
+++ test/manager-test.js
@@ -5,7 +5,27 @@ import User from '../src/data-model/user';
 import Manager from '../src/data-model/manager';
 
 describe('Manager', () => {
-  it('should return true', () => {
-    expect(true).to.equal(true);
+  let manager;
+  beforeEach(() => {
+    manager = new Manager({id: 0, name: "Manager"})
+  });
+
+  describe('Constructor', () => {
+    it('should be a function', () => {
+      expect(Manager).to.be.a('function');
+    });
+
+    it('should create an instance of Manager', () => {
+      expect(manager).to.be.an.instanceof(Manager);
+      expect(manager).to.be.an.instanceof(User);
+    });
+
+    it('should have an id', () => {
+      expect(manager.id).to.deep.equal(0);
+    })
+
+    it('should have a name', () => {
+      expect(manager.name).to.deep.equal("Manager");
+    });
   });
 });
